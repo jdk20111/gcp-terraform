82b1dc9aad3ffbebf8350d3d503682c7d20a2789
Jonathan <jdk20111@gmail.com>
2024-03-31 18:57:22 -0600

    feat: add mtls config

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type    = string
+  default = "ivory-team-416601"
+}
+
+variable "project_number" {
+  type    = number
+  default = 131231448223
+}
--- versions.tf
+++ versions.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.19.0"
+      version = "5.22.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.6.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.22.0"
     }
   }
 }
